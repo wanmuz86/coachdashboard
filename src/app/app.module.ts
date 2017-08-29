9320a585a2985a2dc213ab1c2e35e06d01cf5772
wan muzaffar Wan Hashim <wanmuz86@gmail.com>
2017-08-29 21:27:44 +0800

    add http import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpService} from . './http.service'
+import {HttpModule} from '@angular/http'
  
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    FormsModule
   ],
   providers: [HttpService],
   bootstrap: [AppComponent]
